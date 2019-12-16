810968f00218cf0600efffbb9d19246873d9d0f6
sokolovnnov <sokolovnnov@gmail.com>
2019-12-16 22:42:47 +0300

    Initial commit

---
+++ src/main/java/web/connectivity.java
@@ -0,0 +1,13 @@
+package web;
+
+import java.io.IOException;
+
+public class connectivity extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.getRequestDispatcher("/connectivity.jsp").forward(request, response);
+    }
+}
